b5e94edcb61951563bf065f6d0637d75ac46d54f
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-02-25 10:39:57 +0800

    Windows

--- oop.py
+++ oop.py
@@ -31,13 +31,35 @@ def speech_to_text():
         messagebox.showerror("Error", "Please enter a valid duration")
         return
 
+    messagebox.showinfo(message = "Talk to the mic")
     with sr.Microphone() as source:
-        messagebox.showinfo(message = "Listening...")
         recognizer.adjust_for_ambient_noise(source)
         audio = recognizer.listen(source, phrase_time_limit=duration)
-        messagebox.showinfo(message= "Recognizing...")
         try:
             text = recognizer.recognize_google(audio)
             messagebox.showinfo(message= "You said:\n " + text) 
         except:
             messagebox.showerror(message= "Error, could not recognize your voice")
+
+window = Tk()
+window.geometry("600x400")
+window.title("Text to Speech and Speech to Text")
+title_label = Label(window, text="Text to Speech and Speech to Text").pack()
+
+text_label = Label(window, text="Enter text:").place(x=20, y=30)
+text_entry = Text(window, height=5, width=50)
+text_entry.place(x=110, y=20)
+
+accent_label = Label(window, text="Enter accent:").place(x=20, y=140)
+accent_entry = Entry(window, width=26) 
+accent_entry.place(x=110, y= 140)
+
+duration_label = Label(window, text="Enter duration:").place(x=20, y=180)
+duration_entry = Entry(window, width=26) 
+duration_entry.place(x=110, y= 180) 
+
+button1 = Button(window, text="Text to Speech", command=text_to_speech).place(x=100, y=220)
+button2 = Button(window, text="Language List", command=list_languages).place(x=200, y=220)
+button3 = Button(window, text="Speech to Text", command=speech_to_text).place(x=300, y=220)
+
+window.mainloop() 
